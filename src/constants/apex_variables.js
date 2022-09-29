d0355ddcf331147d0f2feb491dac71fdb9b3f6fe
puopg <michaelchen101@gmail.com>
2022-09-29 08:26:18 -0700

    Updating cut off temp for CO2

--- src/constants/apex_variables.js
+++ src/constants/apex_variables.js
@@ -7,7 +7,7 @@ module.exports = {
     // CO2
     CO2_START_TIME: "10:00",
     CO2_END_TIME: "19:00",
-    CO2_MAX_TEMP_OFF: "82.0",
+    CO2_MAX_TEMP_OFF: "84.0",
     CO2_DELAY_ON_TIME: "01:00",
 
     // CO2 Reactor
