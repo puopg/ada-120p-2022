772608d5a5ed43f6c70fc81aea29f7148d6a51c3
puopg <michaelchen101@gmail.com>
2024-08-02 19:07:41 -0700

    Reducing light time by 1 hour

--- src/constants/apex_variables.js
+++ src/constants/apex_variables.js
@@ -6,7 +6,7 @@ module.exports = {
     
     // CO2
     CO2_START_TIME: "09:00",
-    CO2_END_TIME: "19:00",
+    CO2_END_TIME: "18:00",
     CO2_MAX_TEMP_OFF: "86.0",
     CO2_DELAY_ON_TIME: "01:00",
 
